d8ea7910c93b08457e32d0f3e7e031e6ef7caf7a
GaryVillegas <gary.villegasb@gmail.com>
2025-07-31 12:07:04 -0400

    TODOs

--- src/main/java/com/message/backend/BackendApplication.java
+++ src/main/java/com/message/backend/BackendApplication.java
@@ -9,14 +9,19 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		//TODO: Create chat room with websocket
+		//TODO: P2P Manage with websocket
+		//TODO: Voice message manage
+		//TODO: Deploid to docket
 
 		Dotenv dotenv = Dotenv.configure()
         .filename(".env")
         .ignoreIfMissing()
         .load();
-
-		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		// System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+		// System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		dotenv.get("MONGO_URI");
+		dotenv.get("JWT_SECRET");
 
 		SpringApplication.run(BackendApplication.class, args);
 	}
